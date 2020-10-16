8eb1395fc24c2688d13c0105dca9f5d556e71488
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-10-16 11:24:50 -0700

    initial setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Learning React Hooks in 2020 oct 16 for the first time - functional programming
+
+## Lessons are categorized in branches
+
+### see branches for different lessons
+
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from "./App";
+import "./sass/basic-styles-scss/abstract-and-base.scss";
+
+
+import {
+  HashRouter as Router
+} from "react-router-dom";
+ 
+ReactDom.render(<Router><App/></Router>, document.getElementById("root"));
+
+
+if ('serviceWorker' in navigator) {
+       window.addEventListener('load', () => {
+         navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+           console.log('SW registered: ', registration);
+         }).catch(registrationError => {
+           console.log('SW registration failed: ', registrationError);
+         });
+       });
+     }
