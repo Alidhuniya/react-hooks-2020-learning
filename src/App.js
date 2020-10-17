2496dea6e143a30cbeee62f5335b45527ec14238
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-10-17 15:05:04 -0700

    useEffect renders every time when component re-render

--- src/App.js
+++ src/App.js
@@ -3,34 +3,27 @@ import React, { useState, useEffect, Fragment } from 'react';
  const App = () => {
  const [value, setValue] = useState(0);
 
- const reset = () => {
-   setValue(0);
- }
+ useEffect(() => {
+   console.log("useEffect")
+   console.log(value);
+  //  document.title = `${value}`;
+  if(value >= 1) {
+    document.title = `${value}`;
+  }
+  else {
+    document.title = "Count";
+  }
+ })
 
- const complexIncrease =()=> {
-  //  if you click two times, then it's only increase one time +1 because it doesn't have old state preserve
-  //  setTimeout(() => {
-  //    setValue(value+1)
-  //  }, 2000)
+ console.log("render component");
 
-  // preserve old state 
-
-  setTimeout(() => {
-      setValue((prevState) => {
-        return prevState + 1
-      } )
-  }, 2000)
- }
 
   return (
     <Fragment>
-   <h3>Counter Example with useState</h3>
+   <h3>useEffect Example</h3>
    <h2>{value}</h2>
    <div>
-   <button onClick = {() => setValue(value - 1)} type="button">Decrease</button>
-   <button onClick = {reset} type="button">Reset</button>
    <button onClick = {() => setValue(value + 1)} type="button">Increase</button>
-   <button onClick = {complexIncrease} type="button">Complex Increase</button>
    </div>
    
     </Fragment>
