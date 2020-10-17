847814ed405085eb79339ec57c2a0712862f1972
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-10-16 20:33:17 -0700

    useEffect hook - updating app titles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function App () {
   // Declare a new state variable, which we'll call "count"
+  
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
+
+
+  
 
   return (
     <div>
