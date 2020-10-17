2f5d17b6de4d7f87ab764002a1812890d64d1e0a
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-10-17 10:53:45 -0700

    counter example with useState

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import React, { useState, useEffect, Fragment } from 'react';
 
  const App = () => {
-   const [user, setUser] = useState (
-     {
-       name: "Ali Dhuniya",
-       age: 19,
-       message: "random message"
-     }
-   )
+ const [value, setValue] = useState(0);
 
-   const { name, age, message} = user;
+ const reset = () => {
+   setValue(0);
+ }
 
-   const handleMessageChange = () => {
-      setUser({...user, message: "Hello World"});
-   }
+ const complexIncrease =()=> {
+  //  if you click two times, then it's only increase one time +1 because it doesn't have old state preserve
+  //  setTimeout(() => {
+  //    setValue(value+1)
+  //  }, 2000)
+
+  // preserve old state 
+
+  setTimeout(() => {
+      setValue((prevState) => {
+        return prevState + 1
+      } )
+  }, 2000)
+ }
 
   return (
     <Fragment>
-   <h1>Spread Operator - objects with useState</h1>
+   <h3>Counter Example with useState</h3>
+   <h2>{value}</h2>
    <div>
-     <h1>Name: {name}</h1>
-     <h2>Age: {age}</h2>
-     <h3>Message: {message}</h3>
+   <button onClick = {() => setValue(value - 1)} type="button">Decrease</button>
+   <button onClick = {reset} type="button">Reset</button>
+   <button onClick = {() => setValue(value + 1)} type="button">Increase</button>
+   <button onClick = {complexIncrease} type="button">Complex Increase</button>
    </div>
-   <button onClick = {handleMessageChange} type="button">Change Message</button>
+   
     </Fragment>
 
   );
