ed39634e2121e9ae280f67dd6067e9700c09c45c
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-10-17 09:58:49 -0700

    array of data - useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React, { useState, useEffect, Fragment } from 'react';
 
  const App = () => {
-   const [value, setValue] = useState("change me");
+   const [peoples, setPeoples] = useState ([
+    { id: 1, name: 'john' },
+    { id: 2, name: 'peter' },
+    { id: 3, name: 'susan' },
+    { id: 4, name: 'anna' },
+   ])
 
-   const handleClick = () => {
-    //  if(value === "change me") {
-    //    setValue("changed");
-    //  }
-    //  else {
-    //    setValue("don't changed");
-    //  }
-    
-     value ? setValue("changed") : setValue("don't change")
+   const removePeople = (id) => {
+     let peopleRemoved = peoples.filter((person) => person.id !== id);
+     setPeoples(peopleRemoved);
    }
 
   return (
     <Fragment>
-    <h3>{value}</h3>
-    <button type="button" onClick={handleClick}>Click to Change</button>
+   <h1>Array of Peoples</h1>
+   {
+     peoples.map(people => {
+       const { id, name } = people;
+       return (
+         <div key={id}>
+          <h3>{name}</h3>
+          <button type="button" onClick={() => removePeople(id)}>Clear People</button>
+         </div>
+         
+       )
+     })
+   }
+   <button type="button" onClick={() => setPeoples([])}>Clear All Peoples</button>
     </Fragment>
 
   );
