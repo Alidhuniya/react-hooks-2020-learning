93b145dafa5659b361466b16167a32ffb7d19ef5
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-10-19 19:21:43 -0700

    todo app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import React from 'react';
-import Product from "./components/product/Product";
-
+import React, { useState } from "react";
+import Todo from "./Todo";
+import TodoForm from "./TodoForm";
+ 
 function App() {
-  return <Product />
-}
+  const [todos, setTodos] = useState([
+    { text: "Learn React Hooks", isCompleted: false },
+    { text: "Take a Drive", isCompleted: false },
+    { text: "Play some games", isCompleted: false }
+  ]);
 
+  const handleAddTodo = text => {
+    const newToDos = [...todos, { text }];
+    setTodos(newToDos);
+  };
+
+  const handleComplete = index => {
+    const newToDos = [...todos];
+    newToDos[index].isCompleted = true;
+    setTodos(newToDos);
+  };
+
+  const handleUnfinished = index => {
+    const newToDos = [...todos];
+    newToDos[index].isCompleted = false;
+    setTodos(newToDos);
+  };
+
+  const handleDeleteTodo = index => {
+    const newToDos = [...todos];
+    newToDos.splice(index, 1);
+    setTodos(newToDos);
+  };
+ 
+  return (
+    <React.Fragment>
+      {todos.map((todo, index) => (
+        <Todo todo={todo} key={index} onComplete={handleComplete}
+          index={index} onUnfinished={handleUnfinished} onDeleteTodo={handleDeleteTodo} />
+      ))}
+      <TodoForm onAddTodo={handleAddTodo} />
+    </React.Fragment>
+  );
+}
+ 
 export default App;
