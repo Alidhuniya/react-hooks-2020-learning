06d6bb76d41dfb58cfb169d391d3f46d37e4347e
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-10-20 09:33:47 -0700

    generic fetch with useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
+const useFetch = url => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const [item] = data.results;
+    setData(item);
+    setLoading(false);
+  }, []);
+
+  return { data, loading };
+};
 
 const  App = () => {
   const [count, setCount] = useState(0);
-  const [person, setPerson] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(async() => {
-    const ftch = await fetch("https://api.randomuser.me/");
-    const data = await ftch.json();
-    const item = data.results[0]
-    // console.log(item);
-    setPerson(item)
-    setLoading(false);
-  }, [])
+  const { data, loading } = useFetch("https://api.randomuser.me/");
 
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
-     {loading ? <div>loading</div> : person && <div>{person.name.first}</div>}
+      {loading ? <div>...loading</div> : <div>{data.name.first}</div>}
     </div>
   );
 }
