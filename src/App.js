44855ebe76ffcc0ff1a69eaab56c2aa21727791b
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-10-19 18:43:46 -0700

    add remove cart

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React from 'react';
+import Product from "./components/product/Product";
 
- const App = ({text, maxLength}) => {
-
-  const [hidden, setHidden] = useState(true);
-
-  // If the text is short enough, just render it
-  if (text.length <= maxLength) {
-    return <span>{text}</span>;
-  }
- 
-    return (
-      <>
-       <span>
-      {hidden ? `${text.substr(0, maxLength).trim()} ...` : text}
-      {hidden ? (
-        <a onClick={() => setHidden(false)}> read more</a>
-      ) : (
-        <a onClick={() => setHidden(true)}> read less</a>
-      )}
-    </span>
-      </>
-    );
+function App() {
+  return <Product />
 }
 
 export default App;
